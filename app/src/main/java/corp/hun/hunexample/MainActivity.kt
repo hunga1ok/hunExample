35b83abb6717fe0a1c722348a9a5a28a503cb9a6
Le Van Hung <hunga1ok@gmail.com>
2022-03-23 19:34:20 +0700

    demo coroutines

--- app/src/main/java/corp/hun/hunexample/MainActivity.kt
+++ app/src/main/java/corp/hun/hunexample/MainActivity.kt
@@ -3,14 +3,39 @@ package corp.hun.hunexample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import corp.hun.hunexample.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 
 class MainActivity : AppCompatActivity() {
+    private var count = 0
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        with(binding) {
+            btnCount.setOnClickListener {
+                tvCount.text = count++.toString()
+            }
+            btnDownloadUserData.setOnClickListener {
+                CoroutineScope(Dispatchers.IO).launch {
+                    downloadUserData()
+                }
+            }
+        }
+
         setContentView(binding.root)
+    }
+
+    private suspend fun downloadUserData() {
+
+        withContext(Dispatchers.Main) {
+            for (i in 1..200000) {
+                binding.tvUserMessage.text =
+                    String.format("Downloading user $i in ${Thread.currentThread().name}")
+                delay(10)
+            }
+        }
     }
 }
